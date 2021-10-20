8b0cd77be8e8fc6f0e271d2c7f50b06e8b8893e7
ir <ir@zaq1.pw>
2021-10-20 11:20:56 +0300

    auth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,17 @@
+import NextAuth from 'next-auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import Providers from 'next-auth/providers';
+import prisma from '../../../lib/prisma';
+
+// console.log(process.env);
+
+export default NextAuth({
+	providers: [
+		Providers.Google({
+			clientId: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+		}),
+		// Providers.Credentials({})
+	],
+	adapter: PrismaAdapter(prisma),
+});
